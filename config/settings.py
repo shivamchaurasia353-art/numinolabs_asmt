12accbd8e11ee2e84f7b86e4d80fcc6f9911a589
Shivam Chaurasia <shivam.chaurasia@cantire.com>
2026-05-27 01:22:45 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -161,7 +161,7 @@ CORS_ALLOWED_ORIGINS = [
     origin.strip()
     for origin in os.environ.get(
         'CORS_ALLOWED_ORIGINS',
-        'http://localhost:3000,http://127.0.0.1:3000',
+        'http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001',
     ).split(',')
     if origin.strip()
 ]
